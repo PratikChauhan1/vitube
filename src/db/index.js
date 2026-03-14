079ab320ac78ec40bd360e042e82e4393d4f21cd
Pratik 1435 <202301435@daiict.ac.in>
2026-03-15 00:50:44 +0530

    connect to mongo atlas

---
+++ src/db/index.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose"
+import { DB_NAME } from "../constants.js"
+import dotenv from "dotenv"
+
+dotenv.config({
+    path:'./env'
+}) 
+
+const connectDB = async () =>{
+    try {
+       const connectionInstant = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+       console.log(`\n MongoDB Connected !! DB HOST : ${connectionInstant.connection.host}`);
+       
+    } catch (error) {
+        console.log("Mongo DB connection FAILED :",error);
+        process.exit(1)  
+    }
+}
+
+export default  connectDB
