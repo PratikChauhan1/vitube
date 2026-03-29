1d3cf04cc03655c72bb17d801b2944a92aaa40b7
Pratik 1435 <202301435@daiict.ac.in>
2026-03-29 19:21:32 +0530

    Add refresh token functionality and update routes

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/users.controller.js";
+import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/users.controller.js";
 import { upload } from "../middleware/multer.middleware.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 
@@ -26,4 +26,5 @@ route.route("/login").post(loginUser)
 
 route.route("/logout").post(verifyJWT, logoutUser)
 
+route.route("/refresh-token").post(refreshAccessToken)
 export {route}
